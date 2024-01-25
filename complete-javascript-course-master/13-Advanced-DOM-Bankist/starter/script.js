24c7e9660c789db9d170333222b1f4726c125e3b
Chayanit <chayanit.kitm@gmail.com>
2024-01-25 23:27:50 +0700

    setattribute and setclassname and get value from attribute and set style inline

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -83,6 +83,47 @@ document
     message.parentElement.removeChild(message);
   });
 
+// set inline style
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+// not work with non-standard
+
+logo.alt = 'Beautiful minimalist logo';
+
+console.log(logo.getAttribute('src'));
+
+logo.setAttribute('comapany', 'Bankist');
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
 /*
 //////////////////////////////////////////////////////////////////// Selecting, Creating, and Deleting Elements
 
