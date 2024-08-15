6ba667c6214dc87763eda1bfc71e6d29282a9762
chayanit <chayanit.kitm@gmail.com>
2024-08-15 23:44:34 +0700

    Section 17, Lecutre274 - 276 Top-Level Await, Modern Pattern Modules, CommonJS

---
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,103 @@
+/*
+///////////////////////////////////////
+// Exporting and Importing in ES6 Modules
+
+// Importing module
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+console.log('Importing module');
+// console.log(shippingCost);
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+
+console.log(cart);
+
+///////////////////////////////////////
+TOP Level await
+///////////////////////////////////////
+// console.log('start fetching');
+// fetch(`https://jsonplaceholder.typicode.com/posts`)
+//   .then(res => res.json())
+//   .then(data => console.log(data))
+//   .catch(err => console.error(err.message));
+
+// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// const data = await res.json();
+// console.log(data);
+// console.log('something');
+
+const getLastPost = async function () {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+
+///////////////////////////////////////
+Modern Pattern
+///////////////////////////////////////
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
+///////////////////////////////////////
+ CommonJS Modules
+///////////////////////////////////////
+
+// Export
+export.addToCard = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+}
+
+// Import
+const { addToCard } = require('./shoppingCart.js')
+*/
